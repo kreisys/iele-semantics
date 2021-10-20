1d672e403603347fa1683cd8d7984df56c877153
Shay Bergmann <shaybergmann@gmail.com>
2021-10-20 17:30:06 +0000

    wip

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "A Simple Flake™️";
+
+  inputs.utils.url = "github:kreisys/flake-utils";
+
+  outputs = { self, nixpkgs, utils }: utils.lib.simpleFlake {
+    inherit nixpkgs;
+    systems = [ "x86_64-darwin" "x86_64-linux" ];
+    packages = { hello }: {
+      inherit hello;
+      defaultPackage = builtins.trace self.modules hello;
+    };
+  };
+}
